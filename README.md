00fa097502fa4b319822e9ee2d2b8dca066945c0
Matt Luongo <mhluongo@gmail.com>
2021-02-13 13:55:38 -0500

    Simple Gatsby setup + coming soon page

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # tally.cash
+
+The primary website for Tally, the community-owned Web3 wallet.
+
+## Local development
+
+To build and serve the static site locally
+
+```shell
+yarn instal
+yarn build
+yarn serve
+```
+
+You can visit the rendered site at `https://localhost:9000` 🎉
---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    title: `Tally — The community-owned Web3 wallet`,
+    description: ``,
+    author: `@thesis_co`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Tally — The community-owned Web3 wallet`,
+        short_name: `Tally`,
+        start_url: `/`,
+        background_color: `#fbf9f2`,
+        theme_color: `#fbf9f2`,
+        display: `minimal-ui`,
+        icon: `src/images/tally-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+    `gatsby-plugin-typescript`,
+  ],
+}

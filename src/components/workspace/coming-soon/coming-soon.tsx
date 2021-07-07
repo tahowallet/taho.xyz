eb4f29e2c796b40ec378b573f1cbe83c2c0175b2
yesinsky <barchan.const@gmail.com>
2021-07-08 04:13:17 +0700

    feat(ui): 1. Style coming-soon and join-hunt sections. 2. Update fonts.

--- src/components/workspace/coming-soon/coming-soon.tsx
+++ src/components/workspace/coming-soon/coming-soon.tsx
@@ -3,7 +3,14 @@ import React from "react";
 import './coming-soon.scss';
 
 const ComingSoon = ({ children }: any) => {
-    return <div className="coming-soon">Coming Soon</div>
+    return <>
+        <div className="coming-soon">
+            <div className="coming-soon__btn">
+                <div className="coming-soon__btn-text">Coming soon</div>
+            </div>
+            <div className="coming-soon__info">The wallet for all your wallets.</div>
+        </div>
+    </>
 };
 
 ComingSoon.propTypes = {

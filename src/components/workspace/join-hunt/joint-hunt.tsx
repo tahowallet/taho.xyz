6062d0e75bce10e45c80ffcddf64bb60d3546a11
yesinsky <barchan.const@gmail.com>
2021-07-27 20:55:10 +0300

    Revert "Merge pull request #11 from tallycash/feature/tally-6-email"
    This reverts commit a8188b3efef9f5d23df1d6c513a59779b5ff2c3c, reversing
    changes made to ca8c3a20e25134c5861fe020046b8a11d28dd908.

--- src/components/workspace/join-hunt/joint-hunt.tsx
+++ src/components/workspace/join-hunt/joint-hunt.tsx
@@ -7,10 +7,8 @@ const JoinHunt = ({ children }: any) => {
     return <>
         <div className="join-hunt">
             <div className="join-hunt__text">Get notified to be one of the first to use Tally.</div>
-            <form className="join-hunt__email" action="https://cash.us6.list-manage.com/subscribe/post" method="POST">
-                <input type="hidden" name="u" value="983d6d90b75437835165c3937"/>
-                <input type="hidden" name="id" value="cbe670acb1"/>
-                <input required className="join-hunt__form" placeholder="your@email.com" name="MERGE0" id="MERGE0"></input>
+            <form className="join-hunt__email">
+                <input required className="join-hunt__form" placeholder="your@email.com"></input>
                 <button className="join-hunt__btn action-btn">Join the hunt</button>
             </form>
         </div>

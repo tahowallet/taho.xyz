c90d03fdadd691ec50848a27ea6ce1677a24c335
Matt Luongo <mhluongo@gmail.com>
2021-08-05 16:34:58 -0400

    Always use .ts and .tsx extensions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
-import Layout from "../components/layout/layout"
+import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Workspace from "../components/workspace/workspace"
 
--- src/pages/using-typescript.tsx
+++ src/pages/using-typescript.tsx
@@ -2,7 +2,7 @@
 import React from "react"
 import { PageProps, Link, graphql } from "gatsby"
 
-import Layout from "../components/layout/layout"
+import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 type DataProps = {

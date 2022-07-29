11ce8162ebce85d4255d59e609a4484c5c07493f
Gergo Nagy <gergo.nagy@tally.cash>
2022-07-29 16:45:42 +0200

    deploy twitter pixel

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,5 +35,11 @@ module.exports = {
         siteUrl: "https://tally.cash",
       },
     },
+    {
+      resolve: `gatsby-plugin-twitter-pixel`,
+      options: {
+        pixelId: 'o9kgt',
+      },
+    },
   ],
 };

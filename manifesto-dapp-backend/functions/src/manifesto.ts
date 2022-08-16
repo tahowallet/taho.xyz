1e1acba2364aeaf32eec5f708fa428d7791fe95f
Gergo Nagy <gergo.nagy@tally.cash>
2022-08-17 01:09:25 +0800

    add final manifesto text to sign

--- manifesto-dapp-backend/functions/src/manifesto.ts
+++ manifesto-dapp-backend/functions/src/manifesto.ts
@@ -1,3 +1,12 @@
 export const manifestoMessage = `
-asdf
+Tally Ho Community Values:
+
+1. Access for everyone
+You should have direct access to web3 — no matter where you live.
+
+2. Radical transparency
+All code should be 100% open source — for you to copy, fork, or remix.
+
+3. Full community ownership
+Value must flow transparently to you and the community — not insiders.
 `.trim();

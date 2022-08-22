b1b4206338bb08f03b1ad1d71f53bd154a97f558
Massimo Cairo <cairomassimo@gmail.com>
2022-08-22 10:35:16 +0300

    Update pledge copy
    Closes Petition - copy update #80

--- src/features/Manifesto/ManifestoBody/index.tsx
+++ src/features/Manifesto/ManifestoBody/index.tsx
@@ -48,41 +48,38 @@ export function ManifestoBody() {
         >
           Dear Web3,
         </h1>
-        <h3>
-          It doesn&#8217;t matter what you read or listen to. These days,
-          everyone is talking about Web3.
-        </h3>
+        <h3>We have rights. Let&rsquo;s protect them.</h3>
         <p>
-          But when you look past the headlines, something remarkable is
-          unfolding: Web3 is cleansing itself. It&#8217;s washing out bad actors
-          and clearing space for a new Internet that is built, owned, and
-          powered by communities&#8212;not corporations. As citizens of the new
-          Internet, it&#8217;s time for us to get back to basics and remind
-          ourselves why we&#8217;re here.
+          It doesn&rsquo;t matter what you read or listen to: these days,
+          everyone is talking about Web3. But when you look past the headlines,
+          something remarkable is unfolding: Web3 is fortifying itself.
+          We&rsquo;re washing out bad actors and building the foundation for a
+          new Internet, one that&rsquo;s owned by all of us. It&rsquo;s time to
+          get back to basics and remind ourselves why we&rsquo;re here.
         </p>
-        <p>Why is the Tally Ho community here? It&rsquo;s simple:</p>
-
         <h3>
-          Your Web3 wallet isn&rsquo;t decentralized.
+          Our Web3 wallets aren&rsquo;t decentralized.
           <br />
-          But it should be.
+          It&rsquo;s time to change that.
         </h3>
         <p>
-          You might not know it, but the tool you use to connect to the new
-          Internet&#8212;your trusty, inconspicuous wallet&#8212;is likely
-          controlled by a Web2 megacorporation. Take a look at your wallet.
-          Before you buy, sell, or swap, ask yourself: &#8220;Who&#8217;s
-          monitoring my activity? Can they block me from accessing my assets?
-          Who&#8217;s getting rich here?&#8221; We guarantee you&#8217;ll be
-          surprised by what you find out.
+          ​​Your trusty, inconspicuous wallet is your portal to Web3. But the
+          biggest wallets in our ecosystem are controlled by Web2-style
+          megacorporations, and this curtails our rights as members of the new
+          net. Take a look at your wallet and ask yourself: “Who&rsquo;s
+          monitoring my activity? Can they censor me, block me from accessing my
+          assets, or dictate which protocols or networks I connect to?
+          Who&rsquo;s getting rich here?” You&rsquo;ll be surprised by what you
+          find out.
         </p>
-        <h3>Tally Ho is building the wallet you deserve.</h3>
+        <h3>Tally Ho is building the wallet we deserve.</h3>
         <p>
-          Imagine a Web3 wallet that actually embodies Web3 values&#8212;a
-          wallet that&#8217;s available to anyone, anywhere on Earth, that can
-          finance itself, that&#8217;s accountable to you, not some faceless
-          corporation? Turns out you don&#8217;t have to simply imagine it.
-          You&#8217;ve found it right here.
+          Now imagine a community-owned wallet that actually embodies and
+          protects Web3 values—a wallet that&rsquo;s available to anyone,
+          anywhere on Earth, that can finance itself, that&rsquo;s governed by a
+          DAO, and that&rsquo;s accountable to you, not some faceless
+          corporation. Well, that&rsquo;s Tally Ho: a Web3 wallet for everyone,
+          built to safeguard your assets and your rights.
         </p>
         <hr
           className={css`
@@ -108,8 +105,8 @@ export function ManifestoBody() {
             margin: 2rem 0;
           `}
         >
-          Our community is not just building a Web3 wallet that works.
-          We&#8217;re building a Web3 wallet that we believe in. That means:
+          Our community is not just building a wallet that works. We&rsquo;re
+          building a wallet that we all believe in. That means:
         </p>
         <ul
           className={css`
@@ -125,8 +122,7 @@ export function ManifestoBody() {
             title={<>Access for everyone</>}
             body={
               <>
-                You should have direct access to Web3&#8212;no matter where you
-                live.
+                You should have direct access to Web3—no matter where you live.
               </>
             }
           />
@@ -135,8 +131,8 @@ export function ManifestoBody() {
             title={<>Radical transparency</>}
             body={
               <>
-                All code should be 100% open source&#8212;for you to copy, fork,
-                or remix.
+                All code should be 100% open source—for you to copy, fork, or
+                remix.
               </>
             }
           />
@@ -145,19 +141,20 @@ export function ManifestoBody() {
             title={<>Full community ownership</>}
             body={
               <>
-                Value must flow transparently to you and the community&#8212;not
-                corporate insiders.&#10;
+                Value must flow transparently to you and the community—not
+                corporate insiders.
               </>
             }
           />
         </ul>
         <p>
-          These are your rights as a citizen of Web3, and we&#8217;ve built you
-          a wallet to help defend them.
-        </p>
-        <p>
-          If you&#8217;d like to join our community, sign this Community Pledge
-          with your Tally Ho wallet.
+          These are your rights as a citizen of Web3; our community is building
+          the first wallet designed to protect them.{" "}
+          <strong>
+            If you believe in these values and would like to join our community
+            before the DAO launch, sign this Community Pledge with your Tally Ho
+            wallet and make your support public.
+          </strong>
         </p>
       </div>
     </div>

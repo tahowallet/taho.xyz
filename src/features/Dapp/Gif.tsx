1bbb7d74d51610dd0d738918c60d80532211fb7c
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2023-06-20 17:10:18 +0200

    Add basic gif support

---
+++ src/features/Dapp/Gif.tsx
@@ -0,0 +1,34 @@
+  // @ts-nocheck
+import React, { useEffect, useMemo, useRef } from "react";
+import { Image } from "react-konva";
+import "gifler";
+
+type GifProps = {
+  src: string;
+  x?: number,
+  y?: number
+};
+
+// TODO not sure if gifler is the best library to use here, there is no ts support
+export default function Gif(props: GifProps) {
+  const { src, x, y } = props;
+  const imageRef = useRef(null);
+  const canvas = useMemo(() => {
+    return document.createElement("canvas");
+  }, []);
+
+  useEffect(() => {
+    let anim;
+    window.gifler(src).get((a) => {
+      anim = a;
+      anim.animateInCanvas(canvas);
+      anim.onDrawFrame = (ctx, frame) => {
+        ctx.drawImage(frame.buffer, frame.x, frame.y);
+        imageRef.current?.getLayer().draw();
+      };
+    });
+    return () => anim?.stop();
+  }, [src, canvas]);
+
+  return <Image x={x} y={y} image={canvas} ref={imageRef} />;
+}
